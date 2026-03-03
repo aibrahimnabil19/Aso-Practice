5bdff3fb1178885a679288af51c4bc5433180ae2
aibrahimnabil19 <aibrahimnabil@gmail.com>
2026-03-03 19:00:10 +0100

    add Community component and import Script in layout

---
+++ src/components/Community.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Community = () => {
+  return (
+    <main className="bg-[#F9F9F9]">
+        <section className="bg-[#F9F4F0] mx-auto max-w-5xl rounded-3xl flex flex-col justify-between items-center">
+            
+
+        </section>
+    </main>
+  )
+}
+
+export default Community
